f0539c56ad61cfe35ad67d7eea1b7c07c4183526
MolchanovSergii <118202123+MolchanovSergii@users.noreply.github.com>
2023-02-01 19:29:46 +0000

    Deploying to gh-pages from @ MolchanovSergii/mimino2-teamwork@764ad3a72eb1284486bcbfc6a623854b3916bedd 🚀

--- hotel.ecb55754.js
+++
@@ -1,2 +0,0 @@
-function multiply(){var e;e=1e3*document.getElementById("nights-quantity").value,document.getElementById("outprice").innerHTML=e}
-//# sourceMappingURL=hotel.ecb55754.js.map
--- room2lux.e8cc511e.js
+++ room2lux.e8cc511e.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=room.a444d53f.js.map
+//# sourceMappingURL=room2lux.e8cc511e.js.map
