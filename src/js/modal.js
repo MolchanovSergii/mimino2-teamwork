adda07d27cfaff7c609eac996748977e2dc42a25
thebestfantasy <sergej.fronch@gmail.com>
2023-02-01 08:27:13 +0200

    123

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,25 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open123]'),
+    closeModalBtn: document.querySelector('[data-modal-close123]'),
+    modal: document.querySelector('[data-modal123]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
