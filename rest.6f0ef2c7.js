46b87d3e51a47031d824a79b80ba4deb684448d6
MolchanovSergii <118202123+MolchanovSergii@users.noreply.github.com>
2023-02-03 12:15:40 +0000

    Deploying to gh-pages from @ MolchanovSergii/mimino2-teamwork@87aebe0ae45a293d40bd37a22bb413cb8a60a2aa 🚀

---
+++ rest.6f0ef2c7.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"},slideToClickedSlide:!0,mousewheel:{sensitivity:1,eventsTarget:".swiper"},autoplay:!0,autoplaySpeed:2e3,breakpoints:{320:{slidesPerView:1,spaceBetween:0},1024:{slidesPerView:2,spaceBetween:40},1280:{slidesPerView:2,spaceBetween:40}}});
+//# sourceMappingURL=rest.6f0ef2c7.js.map
